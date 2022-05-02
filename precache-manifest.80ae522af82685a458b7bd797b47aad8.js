753e1416cba964ec5df8b09c28137a1586ad39ad
Shardul <tambeshardul@gmail.com>
2022-05-02 14:18:18 +0530

    Updates

---
+++ precache-manifest.80ae522af82685a458b7bd797b47aad8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5993f3466c94d7e99eb",
+    "url": "/eth-swap-dapp-react/static/css/main.d597e7c4.chunk.css"
+  },
+  {
+    "revision": "a5993f3466c94d7e99eb",
+    "url": "/eth-swap-dapp-react/static/js/main.a5993f34.chunk.js"
+  },
+  {
+    "revision": "8db6b0ccf5950bd804fa",
+    "url": "/eth-swap-dapp-react/static/css/1.af909f26.chunk.css"
+  },
+  {
+    "revision": "8db6b0ccf5950bd804fa",
+    "url": "/eth-swap-dapp-react/static/js/1.8db6b0cc.chunk.js"
+  },
+  {
+    "revision": "443c0bbada773727b3f5",
+    "url": "/eth-swap-dapp-react/static/js/runtime~main.443c0bba.js"
+  },
+  {
+    "revision": "6a34639d02244a755d04d54a71ab580d",
+    "url": "/eth-swap-dapp-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d38c93574b13c502e90ed3504ffc8668.js"
+  "/eth-swap-dapp-react/precache-manifest.80ae522af82685a458b7bd797b47aad8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/eth-swap-dapp-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
